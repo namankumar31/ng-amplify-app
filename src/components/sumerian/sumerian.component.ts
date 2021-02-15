27b89716000b7614f39d5c46ac25e676f58cceab
naman <naman_89@yahoo.com>
2021-02-15 12:06:00 -0500

    add sumerian echarts html 3d scene

--- src/components/sumerian/sumerian.component.ts
+++ src/components/sumerian/sumerian.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { XR } from 'aws-amplify';
 
 @Component({
   selector: 'sumerian',
   templateUrl: './sumerian.component.html',
-  styleUrls: ['./sumerian.component.scss']
+  styleUrls: ['./sumerian.component.scss'],
 })
 export class SumerianComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  async ngOnInit() {
+    await this.loadAndStartScene();
   }
 
+  async loadAndStartScene() {
+    await XR.loadScene('EchartsHtml3d', 'sumerian-scene-dom-id');
+    XR.start('EchartsHtml3d');
+  }
 }
